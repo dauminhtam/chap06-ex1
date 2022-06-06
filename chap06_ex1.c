d00668731662e072cc770242a469047a5144dd4f
dauminhtam <106529964+dauminhtam@users.noreply.github.com>
2022-06-06 14:53:03 +0700

    Create chap06_ex1.c

---
+++ chap06_ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a =10;
+	float b = 24.67892345;
+	char ch = 'A';
+	printf ("\nInteger data = %d", a);
+	printf ("\nFloat data = %f", b);
+	printf ("\nCharacter data = %c", ch);
+	printf ("\nThis print the string");
+	printf ("%s", "\nThis aslo print a string");
+	return 0;
+}
